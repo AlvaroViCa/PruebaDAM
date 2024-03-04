5d759a822118df9de965dbf79a19698ead054639
AlvaroFPCadiz <alvaro.vidal.canas.alu@iesfernandoaguilar.es>
2024-03-04 13:19:40 +0100

    Commit numero X

--- HolaGIT.java
+++ HolaGIT.java
@@ -1,5 +1,8 @@
 public class HolaGIT{
 	public static void main(String[]args){
 		System.out.println("Hola,usando GIT");		
+		System.out.println("Hola que tal");
+		System.out.println("Problemitas");
+		System.out.println("Aro");
 	}
 }
